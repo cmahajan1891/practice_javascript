e77ad431d7422a3419b01f0f4784ca6946db79d3
Chetan Mahajan <chetan_mahajan@gwu.edu>
2017-08-19 14:42:49 -0400

    Update script.js

--- Exercise Files/ch03/script.js
+++ Exercise Files/ch03/script.js
@@ -1,7 +1,9 @@
 var myarray;
 //myarray = ["1","2","3"];
+// Array as an Object type.
 myarray = new Array("1","2","3");
 console.log(myarray);
 
+// The array elements have different data types.
 var mixed = ["1","mixed",true];
 console.log(mixed);
