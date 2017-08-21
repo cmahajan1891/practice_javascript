747752270cb7a801f603cd467b2bc69d796b1174
cmahajan1891 <chetan_mahajan@gwu.edu>
2017-08-21 01:37:24 -0400

    This is a script with name, anonymous and immideatly invoked function definitions.

---
+++ Exercise Files/ch04/script.js
@@ -0,0 +1,43 @@
+// function findBiggestFraction() {
+// console.log("The function is running.");
+//   a>b ? console.log("a: ",a) : console.log("b: ",b);
+// }
+// var a = 3/4;
+// var b = 5/7;
+
+// findBiggestFraction();
+
+
+
+
+// findBiggestFraction(a,b) {
+//   a>b ? console.log("a: ",a) : console.log("b: ",b);
+// }
+
+// function findBiggestFraction(firstF, secondF){
+//   var result;
+//   a>b ? result = ["firstF: ", a] : result = ["secondF: ", b];
+//   return result;
+// }
+//
+// var a = 3/4;
+// var b = 5/7;
+//
+// var fracR = findBiggestFraction(a,b);
+// //console.log(fracR);
+//
+// console.log("firstF: ", a);
+// console.log("secondF: ", b);
+// console.log("Fraction Result:", fracR[0] + "with a value of " + fracR[1] + " is the biggest.");
+
+
+var a = 5/7;
+var b = 18/25;
+
+var biggest = function() {
+  var result;
+  a>b ? result = ["a:", a]: result = ["b:", b];
+  console.log("Biggest: " + result);
+}
+
+biggest();
