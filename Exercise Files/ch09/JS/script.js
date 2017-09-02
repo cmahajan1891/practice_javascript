4e46e83346e83ce8802ed2ddfb4461842a372cb6
cmahajan1891 <chetan_mahajan@gwu.edu>
2017-09-02 17:42:27 -0400

    Defining Event Handlers and Listeners.

---
+++ Exercise Files/ch09/JS/script.js
@@ -0,0 +1,20 @@
+
+const CTA = document.querySelector(".cta a");
+const ALERT = document.querySelector("#booking-alert");
+
+
+CTA.classList.remove("hide");
+ALERT.classList.add("hide");
+
+function reveal (e) {
+  e.preventDefault();
+  CTA.classList.toggle("hide");
+  ALERT.classList.toggle("hide");
+}
+
+//CTA.onclick = reveal;
+//CTA.onclick = console.log("This button was clicked.");
+
+//preventing from only the second event to get fired everytime.
+CTA.addEventListener("click", reveal, false);
+CTA.addEventListener("click", function() {console.log("The button was clicked.");}, false);
