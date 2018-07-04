18912dabd004bfb1e408aa0db9d46514de447ee0
Chetan <chetan_mahajan@gwu.edu>
2018-07-03 22:51:23 -0400

    Javascript language constructs.

---
+++ scripts/script1.js
@@ -0,0 +1,13 @@
+let message = "Hello";
+alert(message);
+//
+
+let name = `Chetan`;
+// These are backticks
+let text = `This is my name : ${name}!`;
+
+let unrwrapped = "unrwrapped text."
+let text1 = "This is unwrapped text " + unrwrapped;
+console.log(text1);
+
+alert(text);
